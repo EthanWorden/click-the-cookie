24b0061ea883c32012fd23fd96fc083086f06cb4
Ethan Worden <ethansworden08@gmail.com>
2026-09-23 20:17:10 -0400

    menu background

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -1,18 +1,22 @@
 package;
 
 import flixel.FlxG;
+import flixel.FlxSprite;
 import flixel.FlxState;
 import flixel.text.FlxText;
 import lime.app.Application;
 
 class MainMenuState extends FlxState
 {
+	var menuBG:FlxSprite = new FlxSprite();
     var versionText:FlxText = new FlxText();
 	var titleText:FlxText = new FlxText();
 
     override public function create()
     {
         super.create();
+		menuBG.loadGraphic("assets/images/menuBG.png");
+		add(menuBG);
         versionText = new FlxText(5, FlxG.height - 20, 0, "v" + Application.current.meta.get("version"), 12);
 		add(versionText);
 		titleText.text = "Click the Cookie!";

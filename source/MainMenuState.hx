b714c6744cce905533f53cd11b79fe22bcf1469a
Ethan Worden <ethansworden08@gmail.com>
2026-09-23 20:14:42 -0400

    title text

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -8,12 +8,18 @@ import lime.app.Application;
 class MainMenuState extends FlxState
 {
     var versionText:FlxText = new FlxText();
+	var titleText:FlxText = new FlxText();
 
     override public function create()
     {
         super.create();
         versionText = new FlxText(5, FlxG.height - 20, 0, "v" + Application.current.meta.get("version"), 12);
 		add(versionText);
+		titleText.text = "Click the Cookie!";
+		titleText.y = 25;
+		titleText.x = 25;
+		titleText.size = 50;
+		add(titleText);
     }
 
     override public function update(elapsed:Float)
